826754ad64f297419fa8ce32dc63574de95757ee
Titya <thitya.yem.photo@gmail.com>
2025-02-03 22:05:47 -0500

    Finished topcategories

--- src/components/CategoryFood.tsx
+++ src/components/CategoryFood.tsx
@@ -1,40 +1,46 @@
-import { Products } from "@/lib";
+import { Products } from "@/constants";
 import { Box, Flex } from "@radix-ui/themes";
 import { Button } from "./ui/button";
 import star from "@/assets/svg/Star.svg";
 
-const CategoryFood = () => {
+type categoryProps = {
+  startIndex: number;
+  itemsToShow: number;
+};
+
+const CategoryFood = ({ startIndex, itemsToShow }: categoryProps) => {
   return (
     <div className="flex justify-between items-center overflow-hidden w-[85%]">
-      {Products.slice(0, 3).map((item) => (
+      {Products.slice(startIndex, startIndex + itemsToShow).map((item) => (
         <div key={item.id} className="w-[200px]">
           <div>
             {/* Image Box */}
-            <Box className="bg-white h-[130px] rounded-t-md">
+            <Box className="bg-white h-[160px] rounded-t-md">
               <img
                 src={item.image}
                 alt="Pet food product"
-                className="w-[80px] h-[120px] pt-2 mx-auto"
+                className="relative top-[20px] left-[60px] w-[80px] h-[120px]"
               />
             </Box>
-
             {/* Info Box */}
             <Box className="bg-[#253239] rounded-b-md">
-              <h4 className="text-gray-300 text-base pl-3">{item.name}</h4>
+              <h4 className="pt-2 text-base pl-3 text-gray-300">{item.name}</h4>
               <Flex
                 direction="column"
                 justify="center"
                 align="center"
                 className="*:text-white"
               >
-                <Flex gapX="6" align="center">
+                <Flex gapX="6" align="center" className="pl-4 pb-2 pt-2 w-full">
                   <p className="font-semibold">${item.price}</p>
                   <Flex gapX="2">
                     <img src={star} alt={`Rating ${item.rating}`} />
                     <span>{item.rating}</span>
                   </Flex>
                 </Flex>
-                <Button>{/* Add To Card */}</Button>
+                <Button className="!text-black bg-[#FAD046] hover:bg-[#ffca1e] py-5 px-11 mb-3">
+                  Add To Cart
+                </Button>
               </Flex>
             </Box>
           </div>
--- src/components/TopCategories.tsx
+++ src/components/TopCategories.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Box, Container } from "@radix-ui/themes";
 import PetFoodCategories from "./PetFoodCategories";
 import rabbitImage from "@/assets/svg/CircleRabbit.svg";
@@ -11,8 +12,36 @@ import CategoryFood from "./CategoryFood";
 import leftArrow from "@/assets/svg/BackwardArrow.svg";
 import rightArrow from "@/assets/svg/ForwardArrow.svg";
 import { Button } from "./ui/button";
+import { Products } from "@/constants";
 
 const TopCategories = () => {
+  const [startIndex, setStartIndex] = useState(0);
+  const [itemsToShow, setItemsToShow] = useState(3); // Default to 3 products
+
+  const handleForward = () => {
+    setStartIndex((prevIndex) =>
+      Math.min(prevIndex + itemsToShow, Products.length - itemsToShow)
+    );
+  };
+
+  const handleBackward = () => {
+    setStartIndex((prevIndex) => Math.max(prevIndex - itemsToShow, 0));
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setItemsToShow(4); // Show 4 products for screens wider than 1024px
+      } else {
+        setItemsToShow(3); // Show 3 products for smaller screens
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Container className="pt-10 bg-[#FAD046]">
       <Box className="relative">
@@ -22,10 +51,9 @@ const TopCategories = () => {
         <img
           src={birds}
           alt="category birds"
-          className="hidden md:inline absolute top-1 w-[150px] xl:w-[180px] "
+          className="hidden md:inline absolute top-1 w-[150px] xl:w-[180px]"
         />
       </Box>
-
       <div className="flex flex-wrap gap-2 justify-between items-center w-2/3 mx-auto mt-10">
         <PetFoodCategories name="Rabbit" image={rabbitImage} />
         <PetFoodCategories name="Cat" image={catImage} />
@@ -33,17 +61,20 @@ const TopCategories = () => {
         <PetFoodCategories name="Bird" image={birdImage} />
         <PetFoodCategories name="Fish" image={fishImage} />
       </div>
-
       <Box>
-        <div className=" w-[189px] h-[20px] bg-[#302B2B] rounded-xl my-8 mx-auto"></div>
+        <div className="w-[189px] h-[20px] bg-[#302B2B] rounded-xl my-8 mx-auto"></div>
         <LinkButton
           link="shop"
           name="Check Our Shop"
           classname="block md:hidden mx-auto text-center"
         />
-
         <Box className="!hidden md:!flex justify-between items-center px-2 h-[310px] bg-[#E3462C] rounded-lg">
-          <Button className="border border-black shadow-md bg-transparent px-2 py-2  hover:bg-transparent rounded-sm">
+          {/* Backward Button */}
+          <Button
+            onClick={handleBackward}
+            disabled={startIndex === 0} // Disable if at the start
+            className="border border-black shadow-md bg-transparent px-2 py-2 hover:bg-transparent rounded-sm"
+          >
             <img
               src={leftArrow}
               alt="Backward Arrow"
@@ -51,8 +82,16 @@ const TopCategories = () => {
               className="invert"
             />
           </Button>
-          <CategoryFood />
-          <Button className="border border-black shadow-md bg-transparent px-2 py-2  hover:bg-transparent rounded-sm">
+
+          {/* Display Current Products */}
+          <CategoryFood startIndex={startIndex} itemsToShow={itemsToShow} />
+
+          {/* Forward Button */}
+          <Button
+            onClick={handleForward}
+            disabled={startIndex + itemsToShow >= Products.length} // Disable if at the end
+            className="border border-black shadow-md bg-transparent px-2 py-2 hover:bg-transparent rounded-sm"
+          >
             <img
               src={rightArrow}
               alt="Forward Arrow"
